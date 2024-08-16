35de3786cb1323021fde2168ee6e93fb60af019a
nishant625 <gangwarnishant625@gmail.com>
2024-08-16 18:46:21 +0530

    made creator dashboard page

--- app/(dashboard)/u/[username]/_components/container.tsx
+++ app/(dashboard)/u/[username]/_components/container.tsx
@@ -1,12 +1,27 @@
-"use client"
+"use client";
 
-interface ContainerProps{
-    children:React.ReactNode
-}
-const Container = ({}:ContainerProps) => {
-  return (
-    <div>Container</div>
-  )
-}
+import { cn } from "@/lib/utils";
+import { useCreatorSidebar } from "@/store/use-creator-sidebar";
+import { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
-export default Container
+interface ContainerProps {
+  children: React.ReactNode;
+}
+const Container = ({ children }: ContainerProps) => {
+  const { collapsed, onCollapse, onExpand } = useCreatorSidebar(
+    (state) => state
+  );
+  const matches = useMediaQuery(`(max-width:1024px)`);
+  useEffect(() => {
+    if (matches) {
+      onCollapse();
+    } else {
+      onExpand();
+    }
+  },[matches,onCollapse,onExpand]);
+
+  return <div className={cn(" flex-1",collapsed?" ml-[70ox]" :" ml-[70px] lg:ml-60")}>{children}</div>;
+};
+
+export default Container;
